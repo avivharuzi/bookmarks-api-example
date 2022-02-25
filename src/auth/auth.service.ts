e6db8cf4310248eaaaec0e49c88e2aa10a785eb5
Aviv Haruzi <avivharuzi@gmail.com>
2022-02-25 08:40:25 +0200

    add signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { hash } from 'argon2';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -9,7 +12,29 @@ export class AuthService {
     return 'I am signed up!';
   }
 
-  signup(): string {
-    return 'I am signed in!';
+  async signup(authDto: AuthDto) {
+    // generate the password hash
+    const password = await hash(authDto.password);
+
+    try {
+      // save new user to db
+      const user = await this.prismaService.user.create({
+        data: {
+          email: authDto.email,
+          password,
+        },
+      });
+
+      delete user.password;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already in use');
+        }
+      }
+      throw error;
+    }
   }
 }
