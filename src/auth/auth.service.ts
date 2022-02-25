d1d098a4003333e3aa1836861e72b85e0a3ad7f8
Aviv Haruzi <avivharuzi@gmail.com>
2022-02-25 08:57:48 +0200

    add signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
-import { hash } from 'argon2';
+import { hash, verify } from 'argon2';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 
@@ -8,8 +8,31 @@ import { AuthDto } from './dto';
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
 
-  login(): string {
-    return 'I am signed up!';
+  async login(authDto: AuthDto) {
+    // find user by email
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        email: authDto.email,
+      },
+    });
+
+    // if user not found
+    if (!user) {
+      throw new ForbiddenException('User not found');
+    }
+
+    // check password
+    const isPasswordValid = await verify(user.password, authDto.password);
+
+    // if password is not valid throw exception
+    if (!isPasswordValid) {
+      throw new ForbiddenException('Email or password is incorrect');
+    }
+
+    delete user.password;
+
+    // return user
+    return user;
   }
 
   async signup(authDto: AuthDto) {
